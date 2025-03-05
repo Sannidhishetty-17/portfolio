7786fc77e2ce20918d4eb285540aec7a97a6b8c5
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-06 00:16:44 +0530

    Add files via upload

---
+++ components/SplineComponent.jsx
@@ -0,0 +1,19 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineComponent() {
+  return (
+    <Spline scene="https://prod.spline.design/vbXwkkAs0PEPv9Cv/scene.splinecode" />
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
