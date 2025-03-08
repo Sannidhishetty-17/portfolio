8312a46f8cb3684d2f33df08da007d1efad18fa4
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-08 07:34:23 +0530

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,13 @@
-import React, { useState } from "react";
-import { RiMenu2Line, RiCloseLine } from "react-icons/ri";
+
 import styles from "./navbar.module.css"; // Adjust path as needed
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  // Toggle menu visibility
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
 
   return (
     <nav className={styles.nav}>
       <div className={styles.Title}>Portfolio</div>
       <ul
-        className={`${styles.sections} ${menuOpen ? styles.show : styles.hide}`}
+        className={styles.sections}
       >
         <a href="#About">
           <li className={styles.section}>About</li>
@@ -29,17 +22,9 @@ const Navbar = () => {
           <li className={styles.section}>Contact</li>
         </a>
       </ul>
-
-      <button
-        className={styles.menuIcon}
-        aria-label={menuOpen ? "Close menu" : "Open menu"}
-        aria-expanded={menuOpen}
-        onClick={toggleMenu}
-      >
-        {menuOpen ? <RiCloseLine size={30} /> : <RiMenu2Line size={30} />}
-      </button>
     </nav>
   );
 };
 
 export default Navbar;
+
